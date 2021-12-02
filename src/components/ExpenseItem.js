5f1a53d0b88e1a5ff97dedda02519380dc9990e4
unknown <ghanshyammoyal058@gmail.com>
2021-12-02 14:09:14 +0530

    Till 34

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28,2021</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
+
